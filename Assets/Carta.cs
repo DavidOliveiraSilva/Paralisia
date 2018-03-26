1a91456f36bc1392e5949febcbbb85bd5b6a2713
David Oliveira <clear844@gmail.com>
2018-03-25 23:26:21 -0300

    Carta e zumbi
    creditos soundtrack

---
+++ Assets/Carta.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Carta : MonoBehaviour {
+	private CartaPr carta;
+	// Use this for initialization
+	void Start () {
+		carta = GetComponentInChildren<CartaPr> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnTriggerStay2D(Collider2D coll){
+		if (coll.gameObject.tag == "Player") {
+			if (Input.GetButtonDown("Fire2")) {
+				ToggleCarta ();
+			}
+		}
+	}
+	void ToggleCarta(){
+		if (carta.gameObject.GetComponent<SpriteRenderer> ().enabled) {
+			carta.gameObject.GetComponent<SpriteRenderer> ().enabled = false;
+		} else {
+			carta.gameObject.GetComponent<SpriteRenderer> ().enabled = true;
+		}
+
+	}
+}
